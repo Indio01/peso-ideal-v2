e8c9f6f71d5b4d6b87f55d88d67aee5dc65108f4
Nodd <124269320+Indio01@users.noreply.github.com>
2023-11-01 13:59:32 +0000

    peso-ideal-v2(01/11)

--- src/screen/main/index.jsx
+++ src/screen/main/index.jsx
@@ -1,57 +1,78 @@
-import { Box, Button, ButtonText, Center, FormControl, FormControlLabel, FormControlLabelText, HStack, Heading, Input, InputField, Switch, Text } from "@gluestack-ui/themed";
+import { Box, Button, ButtonText, Center, FormControl, FormControlLabel, FormControlLabelText, HStack, Heading, Image, Input, InputField, Switch, Text } from "@gluestack-ui/themed";
 import { useState } from "react";
 
-export default function Main(){
-    const [peso, setPeso] = useState("72");
-    const [altura,setAltura] = useState("170");
-    const [sexo, setSexo] = useState(false);
-    const [resultado, setResultado] = useState("");
+import homem from "../../../assets/homem.png";
+import mulher from "../../../assets/mulher.png";
 
-    const calcularHandle = () => {
-        let alturaConvertida = parseInt(altura);
-        let result = 0;
+export default function Main(){
+    const [peso,setPeso] = useState("72")
+    const [altura,setAltura] = useState("1.70")
+    const [sexo,setSexo] = useState(false)
+    const [resultado,setResultado] = useState("")
+
+    const calcularHandle = () =>{
+        let alturaConvertida = parseInt(altura)
+        let result = 0
+
+        if(altura>=2.2){
+            setResultado("Altura deve ser menor que 2.2")
+        }
         if(sexo){
             result = (62.1*alturaConvertida)-44.7
-            //mulher
+            //Mulher
         }else{
+            //Homem
             result = (72.7*alturaConvertida)-58
-            //homem
         }
-
-        setResultado(result)
+        setResultado(Math.round(result))
     }
-    
-    return <Box bg="$primary100" p="$2" h={$1/2} w={300} borderRadius={$md}>
-        <Center h={"$full"}>
-            <Heading>Peso Ideal</Heading>
+
+    const limparHandle = () =>{
+        setResultado("")
+        setSexo(false)
+        setPeso("72")
+        setAltura("1.70")
+    }
+    return (
+        <Box bg="$primary100" p="$5"  h={"$3/4"} w={300} borderRadius="$3xl">
+            <Center h={"$full"}>
+                <Heading>Peso Ideal</Heading>
                 <FormControl>
                     <FormControlLabel>
                         <FormControlLabelText>Peso</FormControlLabelText>
                     </FormControlLabel>
-                    <Input w={$full}>
-                        <InputField value={peso} onChangeText={setPeso} />
+                    <Input w={"$full"}>
+                        <InputField  value={peso} onChangeText={setPeso} keyboardType="numeric"/>
                     </Input>
-
                     <FormControlLabel>
                         <FormControlLabelText>Altura</FormControlLabelText>
                     </FormControlLabel>
-                    <Input w={$full}>
-                        <InputField value={altura} onChangeText={setAltura} />
+                    <Input w={"$full"}>
+                        <InputField value={altura} onChangeText={setAltura} keyboardType="numeric"/>
                     </Input>
-
-                    <HStack w={$full} space="md" justifyContent="center" alignItens="center">
+                    <FormControlLabel>
+                        <FormControlLabelText>Sexo</FormControlLabelText>
+                    </FormControlLabel>
+                    <HStack width={"$full"} space="md" justifyContent="center" alignItems="center">
                         <FormControlLabelText>Homem</FormControlLabelText>
-                        <Switch value={sexo} onValueChange={setSexo} />
+                        <Switch value={sexo} onValueChange={setSexo}/>
                         <FormControlLabelText>Mulher</FormControlLabelText>
                     </HStack>
-
-                    <Button onPress={calcularHandle}>
-                        <ButtonText>Calcular</ButtonText>
-                    </Button>
-                    <Box justifyContent="center" alignItens="center" height={100}>
-                        <Text>Resultado: {resultado}</Text>
+                    <Box justifyContent="space-around" display="flex" flexDirection="row">
+                        <Button onPress={calcularHandle}>
+                            <ButtonText>Calcular</ButtonText>
+                        </Button>
+                        <Button onPress={limparHandle}>
+                            <ButtonText>Limpar</ButtonText>
+                        </Button>
+                    </Box>
+                    <Box justifyContent="space-around" alignItems="center" height={100} flexDirection="row" display="flex">
+                        <Text>Resultado:</Text>
+                        <Text fontWeight="$bold">{resultado}</Text>
                     </Box>
                 </FormControl>
-        </Center>
-    </Box>
+                <Image source={sexo ? mulher : homem} height={'35%'} alt="imagem do tipo do sexo"/>
+            </Center>
+        </Box>
+    )
 }
